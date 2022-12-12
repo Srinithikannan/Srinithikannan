f1a4adb6bcc5d4a3e849baaba0620151b2d4cfcc
Srinithikannan <120240484+Srinithikannan@users.noreply.github.com>
2022-12-12 12:16:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import'./component/styling.css';
+import App from'./App';
+import Photography from "./component/photography";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Photography/>);
+
+ 
